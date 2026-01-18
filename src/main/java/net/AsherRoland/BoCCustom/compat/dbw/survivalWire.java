ccb56ecf418ca584c0f7b7a205c111018ef1d3b6
FusionPixelStudio <asher@fusionpixelstudio.com>
2026-01-17 23:37:06 -0500

    succesfully added valk skies and drivebywire

---
+++ src/main/java/net/AsherRoland/BoCCustom/compat/dbw/survivalWire.java
@@ -0,0 +1,8 @@
+package net.AsherRoland.BoCCustom.compat.dbw;
+
+import edn.stratodonut.drivebywire.network.WireAddConnectionPacket;
+
+
+public class survivalWire extends WireAddConnectionPacket {
+
+}
